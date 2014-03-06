c62c1e00be845cb16c9d88becba6df24e41cebff
delconis <delconisng@gmail.com>
2014-03-05 22:17:04 -0500

    This server save is not fully tested, But looks like it would work. Havening issues with server being left in a Starting Up or Shutting Down game state regardless in previous server save method.

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -1,29 +1,26 @@
-local shutdownAtServerSave = false
-local cleanMapAtServerSave = false
+local function serverSave()
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Now Saveing")
+	player:save()
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Save Complete")
+end
 
-local function finalSave()
-        if shutdownAtServerSave then
-                Game.setGameState(GAME_STATE_SHUTDOWN)
-        end
-        if cleanMapAtServerSave then
-                cleanMap()
-        end
-        saveServer()
+local function thirdServerSaveWarning()
+        broadcastMessage("Server is saving game in one minute. Please go to a safe place.", MESSAGE_STATUS_WARNING)
+        addEvent(serverSave, 60000)
 end
 
 local function secondServerSaveWarning()
-        broadcastMessage("Server is saving game in one minute. Please go to a safe place.", MESSAGE_STATUS_WARNING)
-        addEvent(finalSave, 60000)
+        broadcastMessage("Server is saving game in 5 minute. Please go to a safe place.", MESSAGE_STATUS_WARNING)
+        addEvent(thirdServerSaveWarning, 240000)
 end
 
 local function firstServerSaveWarning()
-        broadcastMessage("Server is saving game in 3 minutes. Please go to a safe place.", MESSAGE_STATUS_WARNING)
-        addEvent(secondServerSaveWarning, 120000)
+        broadcastMessage("Server is saving game in 10 minutes. Please go to a safe place.", MESSAGE_STATUS_WARNING)
+        addEvent(secondServerSaveWarning, 300000)
 end
 
 function onTime(interval)
-        broadcastMessage("Server is saving game in 5 minutes. Please go to a safe place.", MESSAGE_STATUS_WARNING)
-        Game.setGameState(GAME_STATE_STARTUP)
-        addEvent(firstServerSaveWarning, 120000)
+        broadcastMessage("Server is saving game in 30 minutes. Please go to a safe place.", MESSAGE_STATUS_WARNING)
+        addEvent(firstServerSaveWarning, 1200000)
         return not shutdownAtServerSave
 end
