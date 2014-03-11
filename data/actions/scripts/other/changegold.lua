84f5266d4c6e16095156f6f10345bf8cd8c696ee
delconis <delconisng@gmail.com>
2014-03-11 19:11:36 -0400

    My current fix. I think I may need to build from newer sources of the server as I think I may be missing the change type command or the transform command It worked previous to update I pulled on 3/10/14

--- data/actions/scripts/other/changegold.lua
+++ data/actions/scripts/other/changegold.lua
@@ -1,16 +1,24 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if item.itemid == ITEM_GOLD_COIN and item.type == ITEMCOUNT_MAX then
-		doChangeTypeItem(item.uid, item.type - item.type)
-		doPlayerAddItem(cid, ITEM_PLATINUM_COIN, 1)
+	if item.itemid == 2148 and item.type == ITEMCOUNT_MAX then
+		if getPlayerItemCount(cid, 2148) >= ITEMCOUNT_MAX then
+			doPlayerRemoveItem(cid, ITEM_GOLD_COIN, 100)
+			doPlayerAddItem(cid, ITEM_PLATINUM_COIN, 1)
+		end
 	elseif item.itemid == ITEM_PLATINUM_COIN and item.type == ITEMCOUNT_MAX then
-		doChangeTypeItem(item.uid, item.type - item.type)
-		doPlayerAddItem(cid, ITEM_CRYSTAL_COIN, 1)
-	elseif item.itemid == ITEM_PLATINUM_COIN and item.type < ITEMCOUNT_MAX then
-		doChangeTypeItem(item.uid, item.type - 1)
-		doPlayerAddItem(cid, ITEM_GOLD_COIN, ITEMCOUNT_MAX)
+		if getPlayerItemCount(cid, ITEM_PLATINUM_COIN) >= ITEMCOUNT_MAX then
+			doPlayerRemoveItem(cid, ITEM_PLATINUM_COIN, 100)
+			doPlayerAddItem(cid, ITEM_CRYSTAL_COIN, 1)
+		end
+	elseif item.itemid == ITEM_PLATINUM_COIN and item.type <= (ITEMCOUNT_MAX - 1) then
+		if getPlayerItemCount(cid, ITEM_PLATINUM_COIN) >= 1 then
+			doPlayerRemoveItem(cid, ITEM_PLATINUM_COIN, 1)
+			doPlayerAddItem(cid, ITEM_GOLD_COIN, ITEMCOUNT_MAX)
+		end
 	elseif item.itemid == ITEM_CRYSTAL_COIN then
-		doChangeTypeItem(item.uid, item.type - 1)
-		doPlayerAddItem(cid, ITEM_PLATINUM_COIN, ITEMCOUNT_MAX)
+		if getPlayerItemCount(cid, ITEM_CRYSTAL_COIN) >= 1 then
+			doPlayerRemoveItem(cid, ITEM_CRYSTAL_COIN, 1)
+			doPlayerAddItem(cid, ITEM_PLATINUM_COIN, ITEMCOUNT_MAX)
+		end
 	else
 		return FALSE
 	end
